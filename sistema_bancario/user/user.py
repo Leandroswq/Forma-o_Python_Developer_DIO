186cbdb8cc1a46779a17ffaa5b4bf82beded2da0
Leandroswq <leandrobonfim15@hotmail.com>
2022-11-04 14:04:00 -0300

    feat: Criação o método que  de saque do usuário

--- sistema_bancario/user/user.py
+++ sistema_bancario/user/user.py
@@ -1,11 +1,16 @@
 class User:
-    def __init__(self, balance: float, limit: float) -> None:
+    def __init__(
+        self,
+        balance: float,
+        withdraw_limit: float,
+        withdraw_qtd_limit: int = 3,
+    ) -> None:
         self.__balance = balance
-        self.__limit = limit
         self.__statement = []
 
-        self.__withdraw_qtd__ = 0
-        self.__WITHDRAW_QTD_LIMIT__ = 3
+        self.__WITHDRAW_LIMIT = withdraw_limit
+        self.__withdraw_qtd = 0
+        self.__WITHDRAW_QTD_LIMIT = withdraw_qtd_limit
 
     @property
     def balance(self):
@@ -22,16 +27,28 @@ class User:
             "total": f"{self.__balance:.2f}",
         }
 
-        return statement
+        self.__statement.append(statement)
 
     def deposit(self, amount: float) -> None:
         if amount <= 0:
             return print("Deposit needs a positive amount")
         self.__balance += amount
-        self.__statement.append(self.__add_statement__("Deposit", amount))
+        self.__add_statement__("Deposit", amount)
+        print(self.__balance)
+
+    def withdraw(self, amount: float) -> None:
+        if self.__withdraw_qtd >= 3:
+            return print("Daily withdrawal limit reached, transaction denied")
+        if amount > 500:
+            return print("The maximum withdrawal allowed is R$ 500.00")
+        if self.__balance < amount:
+            return print("insufficient funds")
+        self.__balance -= amount
+        self.__add_statement__("Withdraw", amount)
 
 
 if __name__ == "__main__":
-    user = User(500, 300)
-    user.Deposit(300)
+    user = User(500, 300, 3)
+    user.deposit(300)
+    user.withdraw(200)
     print(user.balance, user.statement)
