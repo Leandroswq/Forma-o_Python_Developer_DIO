3fa9beefb289e063b86b939ff7920764cf0b70a6
Leandroswq <leandrobonfim15@hotmail.com>
2022-11-04 16:08:09 -0300

    feat: Cria a função main

---
+++ sistema_bancario/main.py
@@ -0,0 +1,36 @@
+from user.user import User
+
+menu = """
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+=> """
+
+
+def main():
+    user = User(0, 500, 3)
+
+    while True:
+
+        option = input(menu).strip()
+
+        if option in ["d", "D"]:
+            amount = float(input("Valor do depósito "))
+            user.deposit(amount)
+        elif option in ["s", "S"]:
+            amount = float(input("Valor para saque "))
+            user.withdraw(amount)
+        elif option in ["e", "E"]:
+            statement = user.statement
+            print(statement)
+        elif option in ["q", "Q"]:
+            break
+
+        else:
+            print(
+                "Operação invalida, por favor seleciona novamente a operação desejada"
+            )
+
+
+main()
