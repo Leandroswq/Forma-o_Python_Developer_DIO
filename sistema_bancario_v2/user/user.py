9aa33a90c3d79bee8d80d230a58a1426cc8dae46
Leandroswq <leandrobonfim15@hotmail.com>
2022-11-07 13:44:20 -0300

    feat: Adiciona os dados do usuario na classe User

--- sistema_bancario_v2/user/user.py
+++ sistema_bancario_v2/user/user.py
@@ -1,13 +1,41 @@
+from sistema_bancario_v2.account.account import Account
+from typing import List
+
+
 class User:
-    def __init__(self, account) -> None:
-        self.account = account
+    def __init__(self, cpf: str, address: str, account: List[Account]) -> None:
+        self.__cpf = "".join([digit for digit in cpf if digit.isdigit()])
+        self.__address = address
+        self.__account = account
+
+    @property
+    def cpf(self):
+        return self.__cpf
+
+    @property
+    def address(self):
+        return self.__address
+
+    @property
+    def account(self):
+        return self.__account
+
+    def add_account(self, account: Account):
+        self.__account.append(account)
 
 
 if __name__ == "__main__":
     from sistema_bancario_v2.account.checking_account import CheckingAccount
 
-    account = CheckingAccount(500, 300, 3)
-    user = User(account)
-    user.account.deposit(300)
-    user.account.withdraw(amount=200)
-    print(user.account.balance, user.account.statement, sep="\n")
+    account = CheckingAccount(500, 500, 3, withdraw_qtd_limit=5, branch=2)
+    user = User("5d@d5", "ddd", [account])
+    user.account[0].deposit(300)
+    user.account[0].withdraw(amount=200)
+    print(
+        user.account[0].balance,
+        user.account[0].statement,
+        user.account[0].branch,
+        user.account[0].account_number,
+        sep="\n",
+    )
+    print(user.cpf, user.address)
